ef46dffb8e7bc5070acee0913355bdd48cf5be19
mweldon <mweldon@yahoo.com>
2024-08-21 22:03:45 -0700

    Use context object to access core functions

--- webpack.config.js
+++ webpack.config.js
@@ -3,34 +3,34 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: path.join(__dirname, 'src/index.js'),
+    devtool: false,
     output: {
         path: path.join(__dirname, 'dist/'),
-        filename: `index.js`,
+        filename: 'index.js',
+    },
+    resolve: {
+        extensions: ['.js'],
     },
     module: {
         rules: [
             {
-                test: /\.js/,
-                exclude: /node_modules/,
-                options: {
-                    cacheDirectory: true,
-                    presets: [
-                        '@babel/preset-env'
-                    ],
-                },
-                loader: 'babel-loader',
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.html$/,
+                use: { loader: 'html-loader' },
             },
         ],
     },
+    plugins: [],
     optimization: {
         minimize: true,
-        minimizer: [new TerserPlugin({
-            extractComments: false,
-            terserOptions: {
-                format: {
-                    comments: false,
-                },
-            },
-        })],
+        minimizer: [
+            new TerserPlugin({ extractComments: false, terserOptions: { mangle: false } }),
+        ],
+    },
+    performance: {
+        hints: false,
     },
 };
